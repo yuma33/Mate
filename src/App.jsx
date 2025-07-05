20cd6c15391187607230e531d62d4b0c00d01d58
you777777 <691nbt80@gmail.com>
2025-07-06 00:44:59 +0900

    フッターの作成

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Event from  "./components/Event";
+import Footer from "./components/Footer";
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Event />} />
         </Routes>
+        <Footer />
       </div>
     </BrowserRouter>
   );
